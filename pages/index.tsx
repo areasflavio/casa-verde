eb00fd0722c003158191b53090786d50b2f04b86
Flávio Arêas <vflavio9@gmail.com>
2022-04-15 18:58:19 -0300

    :tada: initial next app setup

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Casa Verde</title>
+        <meta
+          name="description"
+          content="Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia."
+        />
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  );
+}
