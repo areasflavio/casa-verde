effe5be05da344a79f5bb39e2d96f7deb1d5c1d9
Flávio Arêas <vflavio9@gmail.com>
2022-04-19 21:33:29 -0300

    feat: fetch data from api

--- src/components/Ofertas/index.tsx
+++ src/components/Ofertas/index.tsx
@@ -1,66 +1,45 @@
+import { useEffect, useState } from 'react';
 import { Container, OfertasGrid } from './styles';
 
+type Planta = {
+	name: string;
+	preco: number;
+	precoFormatado: string;
+	img: string;
+	imgFormatado: string;
+	ordem: number;
+}
+
 export const Ofertas = () => {
+	const [plantas, setPlantas] = useState<Planta[]>([]);
+
+	useEffect(() => {
+		fetch('https://gist.githubusercontent.com/bugan/41d60ffa23fa0c4044cc138bf670780d/raw')
+			.then(response => response.json())
+			.then(data => setPlantas(data.map(item => ({
+				...item,
+				imgFormatado: item.img.replace('01_card', ''),
+				precoFormatado: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.preco)
+			}))));
+	}, []);
+
   return (
     <Container>
 			<span>Conheça nossas</span>
 			<h2>ofertas</h2>
 
 			<OfertasGrid>
-				<li>
-					<img src="images/image-info.png" alt="" />
+				{ plantas.map(planta => (
+					<li key={planta.name}>
+						<img src={`images/plantas/${planta.imgFormatado}.png`} alt={planta.imgFormatado} />
 
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
-				<li>
-					<img src="images/image-info.png" alt="" />
-
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
-				<li>
-					<img src="images/image-info.png" alt="" />
-
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
-				<li>
-					<img src="images/image-info.png" alt="" />
-
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
-				<li>
-					<img src="images/image-info.png" alt="" />
-
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
-				<li>
-					<img src="images/image-info.png" alt="" />
-
-					<div>
-						<h4>Ajuga reptans</h4>
-						<small>R$ 20,00</small>
-						<a href="#">Comprar</a>
-					</div>
-				</li>
+						<div>
+							<h4>{planta.name}</h4>
+							<small>{planta.precoFormatado}</small>
+							<a href="#">Comprar</a>
+						</div>
+					</li>
+				))}
 			</OfertasGrid>
     </Container>
   );
